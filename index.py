ef3d1a9479e99ea3d6b79d7bc996b160fa247883
Your Name <tim@timrich.org>
2020-10-01 15:06:26 +0530

    make it better

--- index.py
+++ index.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, request
 import pymysql
 
+
+# class Database:
+#     def __init__(self):
+#         host = "localhost"
+#         user = "root"
+#         password = "webcreationz@1234"
+#         database = "db1"
+#         self.con = pymysql.connect(host=host, user=user, password=password,
+#                                    database=database, cursorclass=pymysql.cursors.DictCursor)
+#         self.cur = self.con.cursor()
+#
+#     def bmi_height(self):
+#         self.cur.execute("SELECT * FROM bmiheight")
+#         result = self.cur.fetchall()
+#         return result
 app = Flask(__name__)
 
 
@@ -19,29 +34,16 @@ def calculator():
         else:
             bmi = f"Hello {name} Your {bmi} weight is overweight..! you need to follow the healthy tips below"
 
-    class Database:
-        def __init__(self):
-            host = "localhost"
-            user = "root"
-            password = "webcreationz@1234"
-            database = "db1"
-            self.con = pymysql.connect(host=host, user=user, password=password,
-                                       database=database, cursorclass=pymysql.cursors.DictCursor)
-            self.cur = self.con.cursor()
 
-        def bmi_height(self):
-            self.cur.execute("SELECT * FROM bmiheight")
-            result = self.cur.fetchall()
-            return result
 
-    def db_query():
-        db = Database()
-        bmih = db.bmi_height()
-        return bmih
+    # def db_query():
+    #     db = Database()
+    #     bmih = db.bmi_height()
+    #     return bmih
+    #
+    # res = db_query()
 
-    res = db_query()
-
-    return render_template("index.html", bmi=bmi, results=res)
+    return render_template("index.html", bmi=bmi)
 
 
 if __name__ == '__main__':
