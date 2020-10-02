258488ef6601baa114002c7bf839e7b5bb89df3d
Your Name <tim@timrich.org>
2020-10-02 21:58:21 +0530

    make it better

--- index.py
+++ index.py
@@ -2,21 +2,20 @@ from flask import Flask, render_template, request
 import pymysql
 
 
-class MyClass():
-    def __init__(self):
-        DATABASE_URL = "postgres://pxeagzzqrittck:002452867f3a7363d45f46988ec54556c0f4f64872e6bb01555dac64bcaab86f@ec2-3-81-240-17.compute-1.amazonaws.com:5432/dbe706vtsuof73"
-        host = "localhost"
-        user = "root"
-        password = "webcreationz@1234"
-        database = "db1"
-        self.con = pymysql.connect(DATABASE_URL=DATABASE_URL,host=host, user=user, password=password,
-                                   database=database, cursorclass=pymysql.cursors.DictCursor)
-        self.cur = self.con.cursor()
-
-    def bmi_height(self):
-        self.cur.execute("SELECT * FROM bmiheight")
-        result = self.cur.fetchall()
-        return result
+# class MyClass():
+#     def __init__(self):
+#         host = "localhost"
+#         user = "root"
+#         password = "webcreationz@1234"
+#         database = "db1"
+#         self.con = pymysql.connect(host=host, user=user, password=password,
+#                                    database=database, cursorclass=pymysql.cursors.DictCursor)
+#         self.cur = self.con.cursor()
+#
+#     def bmi_height(self):
+#         self.cur.execute("SELECT * FROM bmiheight")
+#         result = self.cur.fetchall()
+#         return result
 app = Flask(__name__)
 
 
@@ -36,15 +35,15 @@ def calculator():
             bmi = f"Hello {name} Your {bmi} weight is overweight..! you need to follow the healthy tips below"
 
 
+    #
+    # def db_query():
+    #     db = MyClass()
+    #     bmih = db.bmi_height()
+    #     return bmih
+    #
+    # res = db_query()
 
-    def db_query():
-        db = MyClass()
-        bmih = db.bmi_height()
-        return bmih
-
-    res = db_query()
-
-    return render_template("index.html", bmi=bmi, results=res)
+    return render_template("index.html", bmi=bmi)
 
 
 if __name__ == '__main__':
